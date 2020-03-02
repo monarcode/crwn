da3f02073a38d6c4472b0304de26aa804f141552
Ugbah Isioma <macbook@Isiomas-MacBook-Pro.local>
2020-03-02 09:29:48 +0100

    homepage components setup + styling

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Directoy from '../../components/directory/directory.Component'
+
+import './homepage.styles.scss'
+
+const Homepage = () => (
+    <div className="homepage">
+        <Directoy />
+    </div>
+)
+
+export default Homepage
